c6ca9092b789bd15a1527421508a7cb2e812b6cc
sahil852963 <sahilsharma83999@gmail.com>
2025-10-15 18:44:39 +0530

    hotels data displayed

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,8 +1,7 @@
 import "./HotelCard.css";
 
-export const HotelCard = ({hotel}) => {
-  
-  const {_id, name, image, rating, price, address, city, state} = hotel;
+export const HotelCard = ({ hotel }) => {
+  const { _id, name, image, rating, price, address, city, state } = hotel;
   // console.log(image)
   return (
     <div className="relative hotelcard-container shadow cursor-pointer">
@@ -10,9 +9,11 @@ export const HotelCard = ({hotel}) => {
         <img className="img" src={image} alt={name} />
         <div className="hotelcard-details">
           <div className="d-flex align-center">
-            <span className="location">{address}, {state}</span>
-            <span>
-              <span>start</span>
+            <span className="location">
+              {address}, {state}
+            </span>
+            <span className="rating d-flex align-center">
+              <span class="material-icons-outlined">star</span>
               <span>{rating}</span>
             </span>
           </div>
@@ -23,11 +24,10 @@ export const HotelCard = ({hotel}) => {
           </p>
         </div>
       </div>
-        <button className="button btn-wishlist absolute d-flex align-center">
-          <span className="material-icons favorite cursor">favorite</span>
-        </button>
-      <div>
-      </div>
+      <button className="button btn-wishlist absolute d-flex align-center">
+        <span className="material-icons favorite cursor">favorite</span>
+      </button>
+      <div></div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -57,12 +57,10 @@ export const Home = () => {
           endMessage={<p className="alert-text">You have seen it all</p>}
         >
           <main className="main d-flex align-center wrap gap-larger">
-            {
-              hotels &&
-                hotels.map((hotel) => (
-                  <HotelCard key={hotel._id} hotel={hotel} />
-                ))
-            }
+            {hotels &&
+              hotels.map((hotel) => (
+                <HotelCard key={hotel._id} hotel={hotel} />
+              ))}
           </main>
         </InfiniteScroll>
       ) : (
