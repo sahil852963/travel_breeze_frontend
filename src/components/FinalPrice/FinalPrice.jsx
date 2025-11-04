4c462533fb2146e3f6d4a5e315a79e7ef3f2aedc
sahil852963 <sahilsharma83999@gmail.com>
2025-11-04 12:19:30 +0530

    latest code

--- src/components/FinalPrice/FinalPrice.jsx
+++ src/components/FinalPrice/FinalPrice.jsx
@@ -1,59 +1,56 @@
-import './FinalPrice.css';
+import "./FinalPrice.css";
 
-export const FinalPrice = ({singleHotel}) => {
-    if(!singleHotel) return;
+export const FinalPrice = ({ singleHotel }) => {
+    if (!singleHotel) return;
     const { _id, price, rating } = singleHotel;
 
-    return(
+    return (
         <div className="price-details-container d-flex direction-column gap shadow">
             <div className="price-rating d-flex align-center justify-space-between">
                 <p>
-                <span className="fs-bold fs-large">Rs. {price}</span> night
+                    <span className="fs-bold fs-large">Rs. {price}</span> night
                 </p>
                 <span className="rating d-flex align-center">
-                <span className="material-icons-outlined">star</span>
-                <span>{rating}</span>
+                    <span className="material-icons-outlined">star</span>
+                    <span>{rating}</span>
                 </span>
             </div>
 
-            
             <div className="d-flex direction-column">
                 <div className="grid-container-two-col selected-dates">
-                <div className="checkin loc-container">
-                    <label className="label">Check in</label>
-                </div>
-                <div className="checkin loc-container">
-                    <label className="label">Check Out</label>
-                </div>
+                    <div className="checkin loc-container">
+                        <label className="label">Check in</label>
+                    </div>
+                    <div className="checkin loc-container">
+                        <label className="label">Check Out</label>
+                    </div>
                 </div>
                 <div className="guests gutter-sm">
-                <p>GUESTS</p>
-                 <span> guests</span>
+                    <p>GUESTS</p>
+                    <span> guests</span>
                 </div>
             </div>
 
             <div>
-                <button
-                className="button btn-reserve btn-primary cursor"
-                >
-                Reserve
+                <button className="button btn-reserve btn-primary cursor">
+                    Reserve
                 </button>
             </div>
 
             <div className="price-distribution d-flex direction-column">
                 <div className="final-price d-flex align-center justify-space-between">
-                <span className="span">Rs. {price} x 2 nights</span>
-                <span className="span">Rd. {price * 2}</span>
+                    <span className="span">Rs. {price} x 2 nights</span>
+                    <span className="span">Rs. {price * 2}</span>
                 </div>
                 <div className="final-price d-flex align-center justify-space-between">
-                <span className="span">Service fee</span>
-                <span className="span">Rd. 200</span>
+                    <span className="span">Service fee</span>
+                    <span className="span">Rs. 200</span>
                 </div>
                 <div className="final-price d-flex align-center justify-space-between">
-                <span className="span">Total</span>
-                <span className="span">Rs. {price * 2 + 200}</span>
+                    <span className="span">Total</span>
+                    <span className="span">Rs. {price * 2 + 200}</span>
                 </div>
             </div>
         </div>
     );
-}
+};
--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -2,42 +2,40 @@ import { useNavigate } from "react-router-dom";
 import "./HotelCard.css";
 
 export const HotelCard = ({ hotel }) => {
-  const { _id, name, image, rating, price, address, city, state } = hotel;
+	const { _id, name, image, rating, price, address, city, state } = hotel;
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const onSingleHotelCardClick = () => {
-    navigate(`hotel/${name}/${address}-${state}/${_id}/reserve`)
-  }
-  
-  // console.log(image)
-  return (
-    <div className="relative hotelcard-container shadow cursor-pointer">
-      <div onClick={onSingleHotelCardClick}>
-        <img className="img" src={image} alt={name} />
-        <div className="hotelcard-details">
-          <div className="d-flex align-center">
-            <span className="location">
-              {address}, {state}
-            </span>
-            <span className="rating d-flex align-center">
-              <span class="material-icons-outlined">star</span>
-              <span>{rating}</span> 
-            </span>
-          </div>
-          <p className="hotel-name">{name}</p>
-          <p className="price-details">
-            <span className="price">Rs. {price}</span>
-            <span>night</span>
-          </p>
-        </div>
-      </div>
-      <button className="button btn-wishlist absolute d-flex align-center">
-        <span className="material-icons favorite cursor">favorite</span>
-      </button>
-      <div></div>
-    </div>
-  );
+	const onSingleHotelCardClick = () => {
+		navigate(`hotel/${name}/${address}-${state}/${_id}/reserve`);
+	};
+
+	// console.log(image)
+	return (
+		<div className="relative hotelcard-container shadow cursor-pointer">
+			<div onClick={onSingleHotelCardClick}>
+				<img className="img" src={image} alt={name} />
+				<div className="hotelcard-details">
+					<div className="d-flex align-center">
+						<span className="location">
+							{address}, {state}
+						</span>
+						<span className="rating d-flex align-center">
+							<span class="material-icons-outlined">star</span>
+							<span>{rating}</span>
+						</span>
+					</div>
+					<p className="hotel-name">{name}</p>
+					<p className="price-details">
+						<span className="price">Rs. {price}</span>
+						<span>night</span>
+					</p>
+				</div>
+			</div>
+			<button className="button btn-wishlist absolute d-flex align-center">
+				<span className="material-icons favorite cursor">favorite</span>
+			</button>
+			<div></div>
+		</div>
+	);
 };
-
-
